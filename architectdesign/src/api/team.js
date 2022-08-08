572315d2c4bf3bd95680610d862ad66941e35b9b
Emmanuel Adedeji <emmans4destiny@gmail.com>
2022-08-08 16:13:56 +0300

    initial commit

---
+++ architectdesign/src/api/team.js
@@ -0,0 +1,34 @@
+import timg1 from '../images/team/img-1.jpg'
+import timg2 from '../images/team/img-2.jpg'
+import timg3 from '../images/team/img-3.jpg'
+import timg4 from '../images/team/img-4.jpg'
+
+
+const Teams = [
+    {
+        Id: '1',
+        tImg:timg1,
+        name:'Akeem Odunmbaku',
+        title:'Creative Director',
+    },
+    {
+        Id: '2',
+        tImg:timg2,
+        name:'Cad Station',
+        title:'Senior Architect',
+    },
+    {
+        Id: '3',
+        tImg:timg3,
+        name:'Biodun Finland',
+        title:'Creative Director',
+    },
+    {
+        Id: '4',
+        tImg:timg4,
+        name:'Akeem Odunmbaku',
+        title:'Art Director',
+    },
+]
+
+export default Teams;
